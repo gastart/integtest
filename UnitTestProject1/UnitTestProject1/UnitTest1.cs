cb5106a950c9a0eb8b37af85376e6c45c74ed7ea
David Joseph Bonnici <david.bonnici@altaro.com>
2018-04-26 14:27:07 +0200

    Add simple unit test. Some are expected to fail whereas others succeed

---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.That(0, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace UnitTestProject2
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod3()
+        {
+            Assert.That(4, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void TestMethod4()
+        {
+            Assert.That(6, Is.EqualTo(6));
+        }
+    }
+}
